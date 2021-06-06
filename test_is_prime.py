48d01bd9123dc9557799668e6465de6d7bd18533
ykato27 <y.kato930@gmail.com>
2021-06-06 17:38:21 +0900

    add is_prime test

---
+++ test_is_prime.py
@@ -0,0 +1,27 @@
+"""
+素数を判定するアルゴリズムのpytest
+
+"""
+import pytest
+
+from is_prime import is_prime_v1 as is_prime
+
+
+@pytest.mark.parametrize(('number', 'expected'), [
+    (2, True),
+    (3, True),
+    (5, True),
+    (7, True),
+])
+def test_is_prime_ok(number, expected):
+    assert is_prime(number) == expected
+
+
+@pytest.mark.parametrize(('number', 'expected'), [
+    (1, False),
+    (4, False),
+    (6, False),
+    (8, False),
+])
+def test_is_prime_ng(number, expected):
+    assert is_prime(number) == expected
